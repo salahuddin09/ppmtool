8bd08b1c2b347c258ece58a6da965d546241ade1
salahuddin09 <salahuddincse09@gmail.com>
2021-08-08 23:48:45 +0600

    Custom Exception Handler

--- src/main/java/com/personal/project/management/tool/ppmtool/services/ProjectService.java
+++ src/main/java/com/personal/project/management/tool/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.personal.project.management.tool.ppmtool.services;
 
 import com.personal.project.management.tool.ppmtool.domain.Project;
+import com.personal.project.management.tool.ppmtool.exceptions.ProjectIdException;
 import com.personal.project.management.tool.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,10 +13,12 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project){
-
-        //Logic
-
-        return projectRepository.save(project);
+        try{
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }catch (Exception e){
+            throw new ProjectIdException("Project ID '"+project.getProjectIdentifier().toUpperCase()+"' already exists");
+        }
     }
 
 }
